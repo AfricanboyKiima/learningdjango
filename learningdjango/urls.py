754440e58de0572fe22626d3c3d5d156b4f646b7
Africanboy Kiima <fadhilkiima@gmail.com>
2023-04-18 12:54:34 +0100

    Pointed my URL conf in urls.py at application level to into the urlpatterns variable in urls.py at project level

--- learningdjango/urls.py
+++ learningdjango/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+#We point our views in the project's urlconf (urls.py)
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
