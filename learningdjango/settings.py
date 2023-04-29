39c8bf762c6f078863245ac735a082165242dca1
Africanboy Kiima <fadhilkiima@gmail.com>
2023-04-29 07:01:56 +0100

    Added gview to list of installed apps

--- learningdjango/settings.py
+++ learningdjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'gview.apps.GviewConfig',
     'tracks.apps.TracksConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
