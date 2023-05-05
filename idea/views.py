a4ca77d45596ffa79a98db7c4027667806aeb715
Africanboy Kiima <fadhilkiima@gmail.com>
2023-05-05 11:23:01 +0100

    Created this app to see how I've mastered the architecture of django

--- idea/views.py
+++ idea/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloworld(request):
-    message = "Hellow world this is kiima from the democratic republic of Congo"
+    message = "Hello world this is kiima from the democratic republic of Congo"
     context = {"detail":message}
     return render(request,"idea/helloworld.html",context)
